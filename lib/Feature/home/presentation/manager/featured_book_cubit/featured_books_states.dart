1e98087db5687074f3ffd927ca7581314323700c
MuhammedAli21 <mohamedmohamed20211@gmail.com>
2025-07-02 05:15:27 +0300

    create featured book cubit

---
+++ lib/Feature/home/presentation/manager/featured_book_cubit/featured_books_states.dart
@@ -0,0 +1,25 @@
+import 'package:booky_app/Feature/home/data/models/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class FeaturedBooksStates extends Equatable {
+  const FeaturedBooksStates();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FeaturedBookInitial extends FeaturedBooksStates {}
+
+class FeaturedBookLoading extends FeaturedBooksStates {}
+
+class FeaturedBookSuccess extends FeaturedBooksStates {
+  final List<BookModel> bookmodel;
+
+  const FeaturedBookSuccess({required this.bookmodel});
+}
+
+class FeaturedBookFailure extends FeaturedBooksStates {
+  final String errorMessage;
+
+  const FeaturedBookFailure({required this.errorMessage});
+}
